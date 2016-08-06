ef50b4a21e8739186f9e471ee8e3d7971f2d51fa
rogerjaffe <rogerjaffe@gmail.com>
2016-08-06 03:55:48 +0000

    Add schedule to demo

--- sample/index.js
+++ sample/index.js
@@ -30,7 +30,11 @@ function displayStudentRoster() {
       if (!result.error) {
         $.each(result.data, function(idx, item) {
           var tr = $('<tr></tr>');
-          tr.append('<td class="id-number">'+item.IDnumber+'</td>');
+          var idTd = $('<td class="id-number"></td>');
+          var a = $('<a href="#" idNumber="'+item.IDnumber+'">'+item.IDnumber+'</a>');
+          a.click(studentIdClickHandler);
+          idTd.append(a);
+          tr.append(idTd);
           tr.append('<td>'+item.Lastname+'</td>');
           tr.append('<td>'+item.Firstname+'</td>');
           // The moment function comes from the moment.js library.  See
@@ -80,5 +84,52 @@ function displayRoomList() {
     }
   });
 }
+
+function displaySchedule(idNumber) {
+  // Set up an AJAX call to Query the database  
+  var query = 'SELECT schedule.*, rooms.Course FROM `schedule` JOIN rooms ON schedule.room=rooms.room WHERE StudentId='+idNumber+' ORDER BY Period';
+  $.ajax({
+    url: '../php/dbQuery.php',      // URL of the DB access page
+    type: 'POST',                   // POST web command
+    dataType: 'json',               // Data will be returned in JSON format
+    data: {                         // These data fields MUST be passed
+      username: 'rogerjaffe',       // Your MySQL username
+      password: '',                 // Leave blank for Cloud 9 access
+      db: 'school',                 // DB name you're connecting to
+      query: query                  // Your DB query defined as a string
+    },
+    // This function is called when the database query is completed
+    // Information passed to the function is:
+    // result.error       TRUE if there was a DB error
+    // result.msg         String with error message if required
+    // result.errorNumber Error number if connection error
+    // result.data        Data retrieved by the query.  Remember that only
+    //                    SELECT queries return data
+    success: function(result) {
+      if (!result.error) {
+        $('.student-schedule-table tbody').empty();      // Clear existing info first
+        $.each(result.data, function(idx, item) {
+          var tr = $('<tr></tr>');
+          tr.append('<td class="room-number">'+item.Period+'</td>');
+          tr.append('<td class="room">'+item.Room+'</td>');
+          tr.append('<td class="course">'+item.Course+'</td>');
+          $('.student-schedule-table tbody').append(tr);
+        });
+      } else {
+        console.log(result.msg);
+      }
+    }
+  });
+}
+
+function studentIdClickHandler(evt) {
+  var element = $(evt.currentTarget);
+  var lastName = $(element.parent().siblings()[0]).text();
+  var firstName = $(element.parent().siblings()[1]).text();
+  $('.student-name').text(firstName+' '+lastName);
+  var idNumber = element.attr('IDnumber');
+  displaySchedule(idNumber);
+}
+
 // When the DOM is loaded call domLoaded
 $(document).ready(domLoaded);
